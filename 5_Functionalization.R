edc548dfac77e50119118bda6b22db7145222ad5
NathanDuda <nathanduda02@gmail.com>
2024-04-04 20:44:18 -0400

    5

--- 5_Functionalization.R
+++ 5_Functionalization.R
@@ -1,29 +1,127 @@
+# Author: Nathan Duda
+# Purpose: 
+#   This script classifies duplicates into their functional classes using CDROM.
 
+# NOTE:
+# from the original CDROM.R script
+# remove the chunks of code dealing with plotting densities.
+# some of my species pairs only have 1 duplicate pair + ancestral copy occurrence for the species pair
+# so error is given when tried to plot density with less than 2 values. 
 
 
 source('./startup.R')
 
 
-# read in duplicate genes with their ancestral copy 
-dups <- read.csv("./Dup_Pairs_Ancestral.tsv", sep="")
+source('./CDROM/CDROM.R')
 
-# read in ortholog pairs 
-ortho_pairs <- read.csv("./Ortholog_Pairs.tsv", sep="")
+dups <- read.csv("./Dup_Pairs_Ancestral.tsv", sep="") %>%
+  mutate(dup_species = gsub("[^A-Z]", "", dup_1),
+         dup_species = gsub("YO", "", dup_species),
+         anc_species = gsub("[^A-Z]", "", ancestral_copy),
+         anc_species = gsub("YO", "", anc_species),
+         
+         species_pair = if_else(dup_species > anc_species,
+                                paste0(dup_species, '_', anc_species),
+                                paste0(anc_species, '_', dup_species)))
 
+
+all_cdrom_output <- data.frame()
+
+for (pair in unique(dups$species_pair)) {
+  
+  species_pair <- strsplit(pair, "_")[[1]]
+  species_1 <- species_pair[1]
+  species_2 <- species_pair[2]
+  
+  
+  
+  # format duplicate genes with their ancestral copy 
+  read.csv("./Dup_Pairs_Ancestral.tsv", sep="") %>%
+    select(dup_1, dup_2, ancestral_copy) %>%
+    filter((grepl(species_1, dup_1) & grepl(species_2, ancestral_copy)) |
+             (grepl(species_2, dup_1) & grepl(species_1, ancestral_copy))) %>%
+    write.table(., file = './CDROM/dup_file.tsv')
+  
+  
+  # format ortholog pairs 
+  orthos <- read.csv("./All_Ortholog_Pairs.tsv", sep="") %>%
+    select(YOgn.x, YOgn.y) %>%
+    filter((grepl(species_1, YOgn.x) & grepl(species_2, YOgn.y)))
+  
+  read.csv("./All_Ortholog_Pairs.tsv", sep="") %>%
+    select(YOgn.x, YOgn.y) %>%
+    filter((grepl(species_1, YOgn.y) & grepl(species_2, YOgn.x))) %>%
+    mutate(YOgn.x_old_temp = YOgn.x,
+           YOgn.x = YOgn.y,
+           YOgn.y = YOgn.x_old_temp) %>%
+    select(-YOgn.x_old_temp) %>%
+    rbind(., orthos) %>%
+    write.table(., file = './CDROM/single_file.tsv')
+  
+  
+  # format expression files
+  read.csv("./Expressed_Expression_Data.tsv", sep="") %>%
+    filter(grepl(species_1, YOgnID)) %>%
+    remove_rownames() %>%
+    column_to_rownames('YOgnID') %>%
+    write.table(., file = './CDROM/expression_file_1.tsv')
+  
+  read.csv("./Expressed_Expression_Data.tsv", sep="") %>%
+    filter(grepl(species_2, YOgnID)) %>%
+    remove_rownames() %>%
+    column_to_rownames('YOgnID') %>%
+    write.table(., file = './CDROM/expression_file_2.tsv')
+  
+  # run CDROM 
+  CDROM(dupFile = './CDROM/dup_file.tsv',
+        singleFile = './CDROM/single_file.tsv',
+        exprFile1 = './CDROM/expression_file_1.tsv',
+        exprFile2 = './CDROM/expression_file_2.tsv',
+        out = './CDROM/Output_',
+        PC = FALSE,
+        useAbsExpr = FALSE)
+  
+  
+  cdrom_output <- read.delim("./CDROM/Output_1.txt")
+  
+  all_cdrom_output <- rbind(all_cdrom_output, cdrom_output)
+
+}
+
+write.table(all_cdrom_output, file = './CDROM/All_CDROM_Output.tsv')
+
+all_cdrom_output <- read.csv("C:/Users/17735/Downloads/Eight_Species/CDROM/All_CDROM_Output.tsv", sep="")
+func <- all_cdrom_output %>%
+  mutate(func = Classification,
+         func = case_when(func == 'Conservation' ~ 'cons',
+                          func == 'Neofunctionalization(Dup1)' ~ 'neo_dup1',
+                          func == 'Neofunctionalization(Dup2)' ~ 'neo_dup2',
+                          func == 'Specialization' ~ 'spec',
+                          func == 'Subfunctionalization' ~ 'sub')) %>%
+  select(-Classification)
+
+
+
+## get relative expression levels 
 # read in expression data 
-raw_exp <- read.csv("./Expression_Data.tsv", sep="")
+raw_exp <- read.csv("./Expressed_Expression_Data.tsv", sep="")
 
-# calculate relative expression values 
-exp <- raw_exp
-rownames(exp) <- exp$YOgnID
-exp <- exp %>% select(-YOgnID)
-exp <- exp / rowSums(exp)
-exp$YOgnID <- rownames(exp)
-exp <- exp[, c("YOgnID", setdiff(names(exp), "YOgnID"))]
+# calculate relative expression values
+exp <- raw_exp %>%
+  remove_rownames() %>%
+  column_to_rownames('YOgnID') %>%
+  {. / rowSums(.)} %>%
+  rownames_to_column('YOgnID')
 
+# write to file 
 write.table(exp,'./Relative_Expression.tsv')
 
-# get exp for dups and ancestral
+
+## classify duplicates as having gained a new function if they have expression in
+## a tissue that the ancestral copy was not expressed in
+
+dups <- dups %>% select(Orthogroup, dup_1, dup_2, ancestral_copy)
+
 dup_1_exp <- exp %>% rename_at(-1, ~paste('dup_1_', ., sep = ''))
 dup_anc_exp <- dups %>%  merge(dup_1_exp, ., by.x = 'YOgnID', by.y ='dup_1') %>% rename(YOgnID = 'dup_1')
 
@@ -33,147 +131,66 @@ dup_anc_exp <- dup_anc_exp %>% merge(dup_2_exp, ., by.x = 'YOgnID', by.y ='dup_2
 anc_exp <- exp %>% rename_at(-1, ~paste('anc_', ., sep = ''))
 dup_anc_exp <- dup_anc_exp %>% merge(anc_exp, ., by.x = 'YOgnID', by.y ='ancestral_copy') %>% rename(YOgnID = 'anc')
 
-# get exp for ortholog pairs 
-ortho_x_exp <- exp %>% rename_at(-1, ~paste('ortho_x_', ., sep = ''))
-ortho_pair_exp <- ortho_pairs %>% merge(ortho_x_exp, ., by.x = 'YOgnID', by.y ='YOgn.x') %>% rename(YOgnID = 'ortho_x')
 
-ortho_y_exp <- exp %>% rename_at(-1, ~paste('ortho_y_', ., sep = ''))
-ortho_pair_exp <- ortho_pair_exp %>% merge(ortho_y_exp, ., by.x = 'YOgnID', by.y ='YOgn.y') %>% rename(YOgnID = 'ortho_y') %>%
-  select(-species.x, -species.y)
+new_exp_neo <- dup_anc_exp
+new_exp_neo$func <- NA
 
-# get combined expression values and after adding, calculate relative expression
-dup_1_exp <- raw_exp %>% rename_at(-1, ~paste('dup_1_', ., sep = ''))
-dups_combined_exp <- dups %>%  merge(dup_1_exp, ., by.x = 'YOgnID', by.y ='dup_1') %>% rename(YOgnID = 'dup_1')
-
-dup_2_exp <- raw_exp %>% rename_at(-1, ~paste('dup_2_', ., sep = ''))
-dups_combined_exp <- dups_combined_exp %>% merge(dup_2_exp, ., by.x = 'YOgnID', by.y ='dup_2') %>% rename(YOgnID = 'dup_2')
 
 tissue_names <- c('f_ac','f_dg','f_go','f_hd','f_re','f_tx','f_wb',
                   'm_ac','m_dg','m_go','m_hd','m_re','m_tx','m_wb')
-for (tissue in tissue_names) {
-  dups_combined_exp <- dups_combined_exp %>%
-    rowwise() %>%
-    mutate("{tissue}" :=  sum(c_across(ends_with(tissue)), na.rm = TRUE))
-}
 
-dups_combined_exp <- dups_combined_exp %>% 
-  select(Orthogroup,all_of(tissue_names))
-
-# calculate relative expression values of combined 
-dups_combined_exp <- as.data.frame(dups_combined_exp) 
-rownames(dups_combined_exp) <- dups_combined_exp$Orthogroup
-dups_combined_exp <- dups_combined_exp %>% select(-Orthogroup)
-dups_combined_exp <- dups_combined_exp / rowSums(dups_combined_exp)
-dups_combined_exp$Orthogroup <- rownames(dups_combined_exp)
-
-# add combined expression values to the dup_anc_exp dataframe  
-dups_combined_exp <- dups_combined_exp %>% rename_at(-ncol(dups_combined_exp), ~paste('d1d2_', ., sep = ''))
-dup_anc_exp <- merge(dups_combined_exp,dup_anc_exp,by='Orthogroup')
-
-# calculate euclidean distance values 
-ed_values <- dup_anc_exp %>%
-  select(-dup_1, -dup_2, -anc) %>%
-  mutate(dup1_a = sqrt(rowSums((select(., starts_with('dup_1')) - select(., starts_with('anc_'))) ^ 2))) %>%
-  mutate(dup2_a = sqrt(rowSums((select(., starts_with('dup_2')) - select(., starts_with('anc_'))) ^ 2))) %>%
-  mutate(d1d2_a = sqrt(rowSums((select(., starts_with('d1d2_')) - select(., starts_with('anc_'))) ^ 2))) %>%
-  select(Orthogroup, dup1_a, dup2_a, d1d2_a)
-
-sc_ed_values <- ortho_pair_exp %>%
-  select(-ortho_x, -ortho_y) %>%
-  mutate(sc_ed = sqrt(rowSums((select(., starts_with('ortho_x')) - select(., starts_with('ortho_y'))) ^ 2))) %>%
-  select(Orthogroup, sc_ed)
-  
-
-# calculate the cutoff ed value 
-iqr <- IQR(sc_ed_values$sc_ed) / 2
-cutoff <- median(sc_ed_values$sc_ed) + iqr
-
-
-# use euclidean distance values to classify into functional groups 
-
-strength_coefficient <- 2
-
-func <- ed_values %>%
-  rowwise() %>%
-  mutate(func = case_when((dup1_a <= cutoff) & (dup2_a <= cutoff) ~ 'conserv',
-                          (dup1_a > cutoff) & (dup2_a <= cutoff) ~ 'neo_dup1',
-                          (dup1_a <= cutoff & dup2_a > cutoff) ~ 'neo_dup2',
-                          (dup1_a > cutoff & dup2_a > cutoff & d1d2_a <= cutoff) ~ 'subfun',
-                          (dup1_a > cutoff & dup2_a > cutoff & d1d2_a > cutoff) ~ 'specializ')) %>%
-  mutate(func_conservative = 
-                case_when((dup1_a <= cutoff*strength_coefficient) & (dup2_a <= cutoff*strength_coefficient) ~ 'conserv',
-                          (dup1_a > cutoff*strength_coefficient) & (dup2_a <= cutoff*strength_coefficient) ~ 'neo_dup1',
-                          (dup1_a <= cutoff*strength_coefficient & dup2_a > cutoff*strength_coefficient) ~ 'neo_dup2',
-                          (dup1_a > cutoff*strength_coefficient & dup2_a > cutoff*strength_coefficient & d1d2_a <= cutoff*strength_coefficient) ~ 'subfun',
-                          (dup1_a > cutoff*strength_coefficient & dup2_a > cutoff*strength_coefficient & d1d2_a > cutoff*strength_coefficient) ~ 'specializ')) %>%
-  mutate(func_strength =
-           case_when(
-             func == 'neo_dup1' & func_conservative == 'neo_dup1' ~ 'strong_neo_dup1',
-             func == 'neo_dup2' & func_conservative == 'neo_dup2' ~ 'strong_neo_dup2',
-             func == 'specializ' & func_conservative == 'specializ' ~ 'strong_specializ',
-             func == 'subfun' & func_conservative == 'subfun' ~ 'strong_subfun',
-             func == 'conserv' & func_conservative == 'conserv' ~ 'strong_conserv',
-           
-             func == 'neo_dup1' & func_conservative != 'neo_dup1' ~ 'weak_neo_dup1',
-             func == 'neo_dup2' & func_conservative != 'neo_dup2' ~ 'weak_neo_dup2',
-             func == 'specializ' & func_conservative != 'specializ' ~ 'weak_specializ',
-             func == 'subfun' & func_conservative != 'subfun' ~ 'weak_subfun',
-             func == 'conserv' & func_conservative != 'conserv' ~ 'weak_conserv'))
-
-# add dup and anc gene ids to func 
-func <- merge(dups,func,by='Orthogroup')
-
-# classify duplicates as having gained a new function if they have expression in a tissue that the ancestral copy was not expressed in
-new_exp_neo <- dup_anc_exp
-new_exp_neo$func <- NA
 
 for (tissue in tissue_names) {
   new_exp_neo <- new_exp_neo %>%
     mutate(func = case_when(
       select(., paste0('dup_2_',tissue)) != 0 & select(., paste0('dup_1_',tissue)) != 0 & 
-        select(., paste0('anc_',tissue)) == 0 ~ 'specializ',
+        select(., paste0('anc_',tissue)) == 0 ~ 'spec',
       select(., paste0('dup_1_',tissue)) != 0 & select(., paste0('anc_',tissue)) == 0 ~ 'neo_dup1',
       select(., paste0('dup_2_',tissue)) != 0 & select(., paste0('anc_',tissue)) == 0 ~ 'neo_dup2',
       T ~ func))
 }
 
-new_exp_neo <- na.omit(new_exp_neo[c('Orthogroup','func')])
+new_exp_neo <- new_exp_neo %>%
+  select(Orthogroup, func) %>%
+  na.omit() 
+
 colnames(new_exp_neo) <- c('Orthogroup','new_exp_func')
 
-#matching_rows <- match(func$Orthogroup, new_exp_neo$Orthogroup)
-#func$func <- ifelse(!is.na(matching_rows), new_exp_neo$func[matching_rows], func$func)
+# attach orthogroups to dups and their functions 
+orthogroups <- read.csv("./Dup_Pairs_Ancestral.tsv", sep="") %>%
+  select(Orthogroup = Orthogroup, Dup1 = dup_1, Dup2 = dup_2, Ancestor = ancestral_copy, pseudo)
+func <- merge(func, orthogroups, by = c('Dup1', 'Dup2', 'Ancestor'))
 
 
+# combine the original functional classification with my new-tissue functional classification 
 func <- func %>%
   left_join(., new_exp_neo, by = 'Orthogroup') %>%
-  mutate(func = case_when(new_exp_func == 'specializ' ~ 'specializ',
-                          func == 'specializ' ~ 'specializ',
-                          func == 'neo_dup1' & new_exp_func == 'neo_dup2' ~ 'specializ',
-                          func == 'neo_dup2' & new_exp_func == 'neo_dup1' ~ 'specializ',
+  mutate(func_actual = case_when(
+                          new_exp_func == 'spec' ~ 'spec',
+                          func == 'spec' ~ 'spec',
+                          func == 'neo_dup1' & new_exp_func == 'neo_dup2' ~ 'spec',
+                          func == 'neo_dup2' & new_exp_func == 'neo_dup1' ~ 'spec',
                           
+                          func == 'cons' & new_exp_func == 'neo_dup1' ~ 'neo_dup1',
+                          func == 'cons' & new_exp_func == 'neo_dup2' ~ 'neo_dup2',
+                          func == 'cons' & new_exp_func == 'spec' ~ 'spec',
                           
-                          (func == 'conserv' | func == 'subfun') & new_exp_func == 'neo_dup1' ~ 'neo_dup1',
-                          (func == 'conserv' | func == 'subfun') & new_exp_func == 'neo_dup2' ~ 'neo_dup2',
-                          (func == 'conserv' | func == 'subfun') & new_exp_func == 'specializ' ~ 'specializ',
-                          !is.na(func) ~ func
+                          func == 'sub' & new_exp_func == 'neo_dup1' ~ 'sub',
+                          func == 'sub' & new_exp_func == 'neo_dup2' ~ 'sub',
+                          func == 'sub' & new_exp_func == 'spec' ~ 'sub',
                           
-                        ))
+                          func == new_exp_func ~ func,
+                          is.na(new_exp_func) & !is.na(func) ~ func,
+                          is.na(new_exp_func) & is.na(func) ~ pseudo))
 
-### new tissue func counts
-how_often_neo_is_newtissue <- func %>%
-  mutate(new_tissue = case_when(
-                          #func == 'specializ' ~ 'specializ',
-                          func == 'specializ' & new_exp_func == 'neo_dup1' ~ 'specializ_newtissue_onedup',
-                          func == 'specializ' & new_exp_func == 'neo_dup2' ~ 'specializ_newtissue_onedup',
-                          
-                          new_exp_func == 'specializ' ~ 'newtissue_specializ',
-                          new_exp_func == 'neo_dup1' & new_exp_func == 'neo_dup2' ~ 'newtissue_specializ',
-                          
-                          #func == 'neo_dup1' ~ 'neo_dup1',
-                          #func == 'neo_dup1' ~ 'neo_dup2',
-                          func == 'neo_dup2' & new_exp_func == 'neo_dup2' ~ 'newtissue_neo',
-                          func == 'neo_dup1' & new_exp_func == 'neo_dup1' ~ 'newtissue_neo'
-  ))
+
+
+
+
+
+
+
+
 
 new_counts <- as.data.frame((table(how_often_neo_is_newtissue$new_tissue))) %>% 
   pivot_wider(names_from='Var1',values_from='Freq')
